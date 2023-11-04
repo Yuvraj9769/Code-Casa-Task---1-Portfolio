9c9ee62a77cad5c14acf0e80ee176c7d60ffecf7
Yuvraj Salte <yuvrajs22it@student.mes.ac.in>
2023-11-04 10:56:29 +0530

    first commit

---
+++ port.js
@@ -0,0 +1,18 @@
+let icon=document.getElementsByClassName("icon");
+icon[0].addEventListener("click",()=>{
+    location.href="https://www.linkedin.com/in/yuvraj-salte-1b0a96270";
+});
+
+icon[1].addEventListener("click",()=>{
+    location.href="https://github.com/Yuvraj9769";
+});
+
+let btn_2=document.getElementsByClassName("btn-2")[0];
+btn_2.addEventListener("click",()=>{
+    location.href="Resume _Yuvraj Salte.pdf";
+});
+
+let btn_1=document.getElementsByClassName("btn-1")[0];
+btn_1.addEventListener("click",()=>{
+    location.href="port2.html";
+});
